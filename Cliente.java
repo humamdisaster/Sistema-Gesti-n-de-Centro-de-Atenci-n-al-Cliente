c4ba4a0c6cec6cb0cb012a6f58082474b13c1ea6
Andi <a.guajardo.cuevas22@gmail.com>
2025-08-28 17:04:02 -0400

    clase cliente agregada

--- Cliente.java
+++ Cliente.java
@@ -1,9 +1,34 @@
 
 public class Cliente {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String id;
+	private String nombre;
+	private String email;
+	
+	//constructor
+	public Cliente(String id, String nombre, String email) {
+		this.id = id;
+		this.nombre = nombre;
+		this.email = email;
 	}
-
+	
+	//getters y setters
+	public String getId() {return id;}
+	public void setId(String id) {this.id = id;}
+	
+	public String getNombre() {return nombre;}
+	public void setNombre(String nombre) {this.nombre = nombre;}
+	
+	public String getEmail() {return email;}
+	public void setEmail(String email) {this.email = email;}
+	
+	//sobrecarga
+	public String info() {
+		return nombre + " (" + id + ")";
+	}
+	
+	public String info(boolean detallado) {
+		if (detallado)
+			return "ID: " + id + " Nombre: " + nombre + " Email: " + email;
+		return info();
+	}
 }
